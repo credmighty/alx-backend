d9a2191d7ad6d241b8f3076afda1e754a0f01602
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-08-20 02:07:48 +0100

    2,3

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@
 
 import csv
 import math
-from typing import List
+from typing import List, Dict, Any
 index_range = __import__('0-simple_helper_function').index_range
 
 
@@ -42,11 +42,16 @@ class Server:
             return []
         return self.dataset()[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """
         the method returns a dictionary
         """
-        assert type(page) == int
-        assert type(page_size) == int
-        assert page > 0
-        assert page_size > 0
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page + 1 <= total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
