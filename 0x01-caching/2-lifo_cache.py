89fbd140019c29977e18fd7f20ad9331d5b48d31
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-08-24 02:21:40 +0100

    used pop inst del

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -8,25 +8,21 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """init method"""
         super().__init__()
-        self.key_indexes = []
+        self.last_put = ""
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item """
         if key and item:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                if key in self.cache_data:
-                    del self.cache_data[key]
-                    self.key_indexes.remove(key)
-            else:
-                del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
-                item_removed = self.key_indexes.pop(self.MAX_ITEMS - 1)
-                print("DISCARD:", item_removed)
+            self.cache_data[key] = item
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            self.cache_data.pop(self.last_put)
+            print("DISCARD:", self.last_put)
 
-        self.cache_data[key] = item
-        self.key_indexes.append(key)
+        if key:
+            self.last_put = key
 
     def get(self, key):
         """Must return the value in self.cache_data linked to key."""
-        if key in self.cache_data:
+        if key and key in self.cache_data:
             return self.cache_data[key]
         return None
