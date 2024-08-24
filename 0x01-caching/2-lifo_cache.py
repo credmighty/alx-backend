b0623960ea8a773196f23476966fb582eca40b7f
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-08-24 02:28:40 +0100

    greater instead greater and equalto

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -7,14 +7,14 @@ class LIFOCache(BaseCaching):
     """class that inherits from BaseCaching and is a caching system"""
     def __init__(self):
         """init method"""
-        super().__init__()
         self.last_put = ""
+        super().__init__()
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item """
         if key and item:
             self.cache_data[key] = item
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             self.cache_data.pop(self.last_put)
             print("DISCARD:", self.last_put)
 
@@ -24,5 +24,5 @@ class LIFOCache(BaseCaching):
     def get(self, key):
         """Must return the value in self.cache_data linked to key."""
         if key and key in self.cache_data:
-            return self.cache_data[key]
+            return self.cache_data.get(key)
         return None
